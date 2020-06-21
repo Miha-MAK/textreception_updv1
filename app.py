852ccc4016187985fc8d94a4d0a985097f23545d
Miha-Mak <66954500+Miha-MAK@users.noreply.github.com>
2020-06-21 14:25:52 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import pytz
 
 bot = telebot.TeleBot("1251300918:AAHtD1W2Clz294i5r1haITmjH03NfYB7Mb0")
 chat_for = "-1001394622167"
-dic = {
-    message.from_user.id: "1"
-    }
+
+dic = {}
+
 print("start")
 @bot.message_handler(commands=['start'])
 def any_msg(message):
-    
-  
+
+
 
     keyboard = types.InlineKeyboardMarkup()
     callback_button_yes = types.InlineKeyboardButton(text="Да, конечно✅", callback_data=f"yes{message.from_user.id}")
@@ -30,15 +30,17 @@ def reply_msg(message):
     if len(message.text) > 20:
 
         bot.forward_message(chat_for,message.from_user.id, message.message_id)
-
         global now
         now = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%d%H%M")
+        dic[message.from_user.id] = now
+
+        print(dic)
+
+
 
         bot.send_message(message.chat.id, text = """Сообщение успешно отправлено✅
 Вы можете делать публикацию через 30 минут.""")
-        print(dic)
-        dic[message.from_user.id] = "false"
-        sleep(5)
+
         bot.send_message(message.chat.id, text = """<b>🔥🔥🔥ВНИМАНИЕ! Впервые в секторе рекламы!
 
 📌Теперь вы можете мгновенно публиковать свои объявления на нашем канале.
@@ -55,17 +57,22 @@ https://t.me/firechannel1
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data.startswith("yes"):
-        #bot.send_message(call.message.chat.id, text = "Хорошо, напишите текст ( Можете использовать ссылки, емодзи, и т.д )")
         global ID
         ID = str(call.data.replace("yes","")) # ID клиента
 
-        if "true" in dic.values():
+
+
+        if call.message.chat.id not in dic.keys():
+            s = bot.send_message(call.message.chat.id, text = "Напишите текст поста...📝")
+            bot.register_next_step_handler(s,reply_msg) # Переходим в reply_msg
+
+        elif int(datetime.now(pytz.timezone("Europe/Moscow")).strftime("%d%H%M")) - int(dic[call.message.chat.id]) > 30:
             s = bot.send_message(call.message.chat.id, text = "Напишите текст поста...📝")
             bot.register_next_step_handler(s,reply_msg) # Переходим в reply_msg
         else:
-            #time = 30 - (int(datetime.now(pytz.timezone("Europe/Moscow")).strftime("%d%H%M")) - int(now))
-            bot.send_message(call.message.chat.id, text = """Вы уже отправили сообщение.✅
-Подождите {} минут и повторите попытку.""", parse_mode = 'HTML')
+             time = 30 - (int(datetime.now(pytz.timezone("Europe/Moscow")).strftime("%d%H%M")) - int(now))
+             bot.send_message(call.message.chat.id, text = """Вы уже отправили сообщение.✅
+Подождите {} минут и повторите попытку.""".format(time), parse_mode = 'HTML')
 
 
 
